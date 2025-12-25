21225144b1c338c4fdaab553be1024be4f2a765b
mavagoncalves <mavagoncalves@gmail.com>
2025-12-25 14:15:03 +0100

    initial set up

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FateRank;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace FateRank;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
